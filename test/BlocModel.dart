7ef35eebab709daf85fe508a3cb143b6058a42f7
nyingi <nyingimaina@gmail.com>
2020-04-10 09:43:39 +0300

    fixed code

--- test/BlocModel.dart
+++ test/BlocModel.dart
@@ -1,6 +1,9 @@
+import 'package:blocstar/ActionState.dart';
 import 'package:blocstar/BlocModelBase.dart';
 
 class BlocModel extends BlocModelBase<BlocModel> {
+  BlocModel(Function(ActionState) onAppStateChanged) : super(onAppStateChanged);
+
   @override
   BlocModel merge() {}
 }
