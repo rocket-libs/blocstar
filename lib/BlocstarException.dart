f59796b82eafe0b13c895ba2acf68602798ba648
nyingi <nyingi.maina@gmail.com>
2020-05-31 10:12:25 +0300

    Removed beta tag on version 1.2.3

--- lib/BlocstarException.dart
+++ lib/BlocstarException.dart
@@ -2,4 +2,13 @@ class BlocstarException implements Exception {
   final dynamic value;
 
   BlocstarException(this.value);
+
+  @override
+  String toString() {
+    if (value != null) {
+      return value.toString();
+    } else {
+      return "Blocstar caught an unspecified error.\n\n${super.toString()}";
+    }
+  }
 }
